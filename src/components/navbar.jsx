7795f8cc76864ef6cce8f31dd1d6d9479b5e1119
Banky <Olowoyeyesnr@gmail.com>
2023-10-21 20:41:06 +0100

    fixing the nav button links 2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Page404 from "../pages/404/404";
-import Profile from "../pages/Profile/Profile";
-import Question from "../pages/Question/Question";
-import Repos from "../pages/Repos/repos";
 
 
 function Navbar() {
@@ -55,13 +51,13 @@ function Navbar() {
             <Link onClick={handleBack} to="/" className="small small-Repos-link">
               Home
             </Link>
-            <Link onClick={handleBack} to="/Repos" className="small small-Repos-link">
+            <Link onClick={handleBack} to="Repos" className="small small-Repos-link">
               Repos
             </Link>
-            <Link onClick={handleBack} to="/404" className="small small-Repos-link">
+            <Link onClick={handleBack} to="404" className="small small-Repos-link">
               Error 404
             </Link>
-            <Link onClick={handleBack} to="/Profile" className="small small-profile">
+            <Link onClick={handleBack} to="Profile" className="small small-profile">
               Profile
             </Link>
           </div>
