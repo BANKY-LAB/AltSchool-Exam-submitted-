3e5d2554598dd0d160c6db8c783fdb46d2d7ed67
Banky <Olowoyeyesnr@gmail.com>
2023-10-22 20:53:35 +0100

    fixed the repo page

--- src/pages/Repos/repos.jsx
+++ src/pages/Repos/repos.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/navbar";
-import Page404 from "../404/404";
 
 import "./repos.css";
 
